3b28532cf47968b493054366d9484773a2483402
ryusei1068 <nhrryi@gmail.com>
2022-02-06 10:23:58 +0900

    generate deck

--- card.go
+++ card.go
@@ -15,9 +15,26 @@ func new_card(value string, suit string, intValue int32) Card {
 	return card
 }
 
-func main() {
-	fmt.Println("Plaing cards")
-	card := new_card("2", "♡", 2)
-	fmt.Println(card)
+func getCard(card Card) string {
+	return card.suit + card.value + string(card.intValue)
+}
 
+func generateDeck() []Card {
+	var deck []Card
+	values := []string{"A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"}
+	suits := []string{"♡", "♤", "♢", "♧"}
+
+	for _, suit := range suits {
+		for i, value := range values {
+			intValue := i + 1
+			deck = append(deck, new_card(value, suit, int32(intValue)))
+		}
+	}
+	return deck
+}
+
+func main() {
+	fmt.Println("Playing cards")
+	deck := generateDeck()
+	fmt.Println(deck)
 }
